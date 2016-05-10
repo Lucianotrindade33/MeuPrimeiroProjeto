e06db87f85047e3e6f24fbe4a1c1421cc1657c21
unknown <tecnico@lab304-01.epfserver.com.br>
2016-05-09 22:04:54 -0300

    foi adicionado uma tag para valor somado

--- src/Principal.java
+++ src/Principal.java
@@ -11,7 +11,8 @@ public class Principal {
 		 calculadora.somar(10,50);
 		 calculadora.somar(20,10);
 		
-		JOptionPane.showMessageDialog(null ,calculadora.getValor());
+		JOptionPane.showMessageDialog(null ,
+				"o valor somado é : "+calculadora.getValor());
 		calculadora.dividir();
 		calculadora.subtrair();
 		
